7d68c64aee7628746228534601564f5e0305beda
Raj Gupta <74600890+rajgupta23@users.noreply.github.com>
2021-11-17 12:20:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,15 @@ int main() {
 		node * temp = getNode(i);
 		insert(t, temp);
 	}
+	printf("%d\n", blackHieght(t));
+	preorder(t, t->root);
+	printf("\n");
+	postorder(t, t->root);
+	printf("\n");
 	inorder(t, t->root);
-
+	printf("\n");
+	printf("max = %d \n", maximum(t));
+	printf("max = %d \n", minimum(t));
+	search(t, 5) ? printf("found\n") : printf("Not found\n");
 	return 0;
 }
