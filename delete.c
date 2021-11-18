6fd8f52b667ce5e0f36d9e19ba75045a5e5aa745
Raj Gupta <74600890+rajgupta23@users.noreply.github.com>
2021-11-18 22:53:45 +0530

    Update delete.c

--- delete.c
+++ delete.c
@@ -18,130 +18,125 @@ typedef struct Tree {
 	node *NIL;
 } Tree;
 
-void left_rotate(Tree *t, node *x){}
+void left_rotate(Tree *t, node *x) {}
 void right_rotate(Tree *t, node *x) {}
 
 void transplant(Tree *t, node *a, node *b)
 {
-    if(a->parent == t->NIL)
-        t->root = b;
-    else if(a == a->parent->left)
-        a->parent->left = b;
-    else
-        a->parent->right = b;
-    b->parent = a->parent;
+	if (a->parent == t->NIL)
+		t->root = b;
+	else if (a == a->parent->left)
+		a->parent->left = b;
+	else
+		a->parent->right = b;
+	b->parent = a->parent;
 }
 node* treeMin(Tree* t, node *z) {  // to find minimum value in tree
 	node * temp = z;
-	if (temp == t->NIL)return NULL; // tree is empty
 	while (temp->left != t->NIL)temp = temp->left;
 	return temp;
 }
-void deleteFixup(Tree *t, node *x, bool n)
+void deleteFixup(Tree *t, node *x)
 {
-	node *w;
-    node *temp = x;
+	// node *w;
+	// node *temp = x;
 	while ((x != t->root) && (x->color == Black))
 	{
 		if (x == x->parent->left) {
-			w = x->parent->right;
+			node * w = x->parent->right;
 			if (w->color == Red) {
 				w->color = Black;
 				x->parent->color = Red;
 				left_rotate(t, x->parent);
 				w = x->parent->right;
 			}
-			if ((w->left->color == Black) && (w->right->color == Black)) {
+			if (w->left->color == Black && w->right->color == Black) {
 				w->color = Red;
 				x = x->parent;
 			}
-			else if (w->right->color == Black) {
-				w->left->color = Black;
-				w->color = Red;
-				right_rotate(t, w);
-				w = x->parent->right;
-			}
-		 
+			else {
+				if (w->right->color == Black) {
+					w->left->color = Black;
+					w->color = Red;
+					right_rotate(t, w);
+					w = x->parent->right;
+				}
 				w->color = x->parent->color;
 				x->parent->color = Black;
 				w->right->color = Black;
 				left_rotate(t, x->parent);
 				x = t->root;
-			
-
+			}
 		}
 
 		else { // x == x->parent->right
-			w = x->parent->left;
+			node * w = x->parent->left;
 			if (w->color == Red) {
 				w->color = Black;
 				x->parent->color = Red;
 				right_rotate(t, x->parent);
 				w = x->parent->left;
 			}
-			if ((w->left->color == Black) && (w->right->color == Black)) {
+			if (w->right->color == Black && w->left->color == Black) {
 				w->color = Red;
 				x = x->parent;
 			}
-			else if (w->left->color == Black) {
-				w->right->color = Black;
-				w->color = Red;
-				left_rotate(t, w);
-				w = x->parent->left;
-			}
-		 
+			else {
+				if (w->left->color == Black) {
+					w->right->color = Black;
+					w->color = Red;
+					left_rotate(t, w);
+					w = x->parent->left;
+				}
 				w->color = x->parent->color;
 				x->parent->color = Black;
 				w->left->color = Black;
 				right_rotate(t, x->parent);
 				x = t->root;
-			
+			}
+
 
 		}
 	}
 	x->color = Black;
-    if(n = 1){
-        if (temp == temp->parent->left)
-			temp->parent->left = t->NIL;
-		if (temp == temp->parent->right)
-			temp->parent->right = t->NIL;
-    }
 
 }
 void delete(Tree *t, int k)
 {
-	node* z = t->root;
+	node* z = malloc(sizeof(node));
+	z = t->root;
 	while ((z->data != k) && (z != t->NIL)) {
 		if (z->data < k)z = z->right;
 		else z = z->left;
 	}
 	node *x;
 	node *y = z;
-    bool n = 0;
+	// bool n = 0;
 	bool yOriginalColor = y->color;
 
-	if ((z->left == t->NIL) && (z->right != t->NIL)) {
+	if (z->left == t->NIL) {
 		x = z->right;
 		transplant(t, z, z->right);
 	}
-	else if ((z->right == t->NIL) && (z->left != t->NIL)) {
+	else if (z->right == t->NIL) {
 		x = z->left;
 		transplant(t, z, z->left);
 	}
-	else if ((z->right == t->NIL) && (z->left == t->NIL)) {
-		/*if (z == z->parent->left)
-			z->parent->left = t->NIL;
-		if (z == z->parent->right)
-			z->parent->right = t->NIL;*/
-		x = z;
-        k = 1;
-	}
+	// else if ((z->right == t->NIL) && (z->left == t->NIL)) {  /// we dont need it
+	// 	if (z == z->parent->left)
+	// 		z->parent->left = t->NIL;
+	// 	if (z == z->parent->right)
+	// 		z->parent->right = t->NIL;
+	// 	x = z;
+	// 	k = 1;
+	// }
 	else {
 		y = treeMin(t, z->right);
-		yOriginalColor = y->color;
+		bool y_orignal_color = y->color;
 		x = y->right;
-		if (y->parent == z)
-			x->parent = y;
+		if (y->parent == z) {
+			x->parent = z;
+		}
 		else {
 			transplant(t, y, y->right);
 			y->right = z->right;
@@ -153,153 +148,5 @@ void delete(Tree *t, int k)
 		y->color = z->color;
 	}
 	if (yOriginalColor == Black)
-		deleteFixup(t, x, n);
+		deleteFixup(t, x);
 }
-
-int main()
-{ 
-    return 0;
-}
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define Red 0
-#define Black 1
-
-struct node{
-	int val;
-	bool color;
-	struct node *parent;
-	struct node *left;
-	struct node *right;
-};
-struct node *ROOT;
-struct node *NILL;
-
-void leftRotate(struct node *x)
-{
-
-}
-void rightRotate(struct node *x)
-{
-
-}
-struct node *treeMin(struct node *z)
-{
-
-}
-void transplant(struct node *a, struct node *b)
-{
-    if(a->parent == NILL)
-        ROOT = b;
-    else if(a == a->parent->left)
-        a->parent->left = b;
-    else
-        a->parent->right = b;
-    a->parent = a->parent;
-}
-void deleteFixup(struct node *x)
-{
-    struct node *w;
-    while((x != ROOT)&&(x->color == Black))
-    {
-        if(x == x->parent->left){
-            w = x->parent->right;
-            if(w->color == Red){
-                w->color = Black;
-                x->parent->color = Red;
-                leftRotate(x->parent);
-                w = x->parent->right;
-            }
-            if((w->left->color == Black)&&(w->right->color == Black)){
-                w->color = Red;
-                x = x->parent;  
-            }
-            else if(w->right->color == Black){
-                w->left->color = Black;
-                w->color = Red;
-                rightRotate(w);
-                w = x->parent->right;
-            }
-            else{
-                w->color = w->parent->color;
-                w->parent->color = Black;
-                w->right->color = Black;
-                leftRotate(x->parent);
-                x = ROOT;
-            }
- 
-        }
-        
-        else{// x == x->parent->right
-            w = x->parent->left;
-            if(w->color == Red){
-                w->color = Black;
-                x->parent->color = Red;
-                rightRotate(x->parent);
-                w = x->parent->left;
-            }
-            if((w->left->color == Black)&&(w->right->color == Black)){
-                w->color = Red;
-                x = x->parent;  
-            }
-            else if(w->left->color == Black){
-                w->right->color = Black;
-                w->color = Red;
-                leftRotate(w);
-                w = x->parent->left;
-            }
-            else{
-                w->color = w->parent->color;
-                w->parent->color = Black;
-                w->left->color = Black;
-                rightRotate(x->parent);
-                x = ROOT;
-            }
-            
-        }
-    }
-	x->color = Black;   
-}
-void delete(struct node *z)
-{
-    struct node *x;
-    struct node *y = z;
-    bool yOriginalColor = y->color;
-
-    if(z->left == NILL){
-        x = z->right;
-        transplant(z,z->right);
-    }
-    else if(z->right == NILL){
-        x = z->left;
-        transplant(z,z->left);
-    }
-    else{
-        y = treeMin(z->right);
-        yOriginalColor = y->color;
-        x = y->right;
-        if(y->parent == z)
-            x->parent = y;
-        else{
-            transplant(y, y->right);
-            y->right = z->right;
-            y->right->parent = y;
-        }
-        transplant(z, y);
-        y->left = z->left;
-        y->left->parent = y;
-        y->color = z->color;
-    } 
-    if(yOriginalColor == Black)
-        deleteFixup(x);
-}
-
-
-int main()
-{
-    
-    return 0;
-}*/
