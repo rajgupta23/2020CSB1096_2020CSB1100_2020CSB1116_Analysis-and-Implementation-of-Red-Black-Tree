cb1c39d7ff209838041020082b65f242d267c8c2
Raj Gupta <74600890+rajgupta23@users.noreply.github.com>
2021-11-18 23:21:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,23 +5,46 @@
 
 int main() {
 	Tree *t = getRBTree(); // initially t-->nil tree
-
-	for (int i = 0; i < 10; i++) {
-		node * temp = getNode(i);
-		insert(t, temp);
+	int choice, i, d, key;
+	while (1) {
+		printf("Enter:\n1.Insertion\n2.Deletion\n3.search\n4.Black Height\n5.Inorder\n6.Postorder\n7.preorder\n8.Quit\n");
+		scanf("%d", &choice);
+		if (choice == 1) {
+			printf("Enter value you want to insert : ");
+			scanf("%d", &i);
+			node* temp = getNode(i);
+			insert(t, temp);
+		}
+		else if (choice == 2) {
+			printf("Enter value you want to delete : ");
+			scanf("%d", &d);
+			delete(t, d);
+		}
+		else if (choice == 3) {
+			printf("Enter value you want to search : ");
+			scanf("%d", &key);
+			if (search(t, key))printf("Element is present\n");
+			else printf("Element is not present\n");
+		}
+		else if (choice == 4) {
+			printf("Black Height of current Red Black tree is %d\n", blackHieght(t));
+		}
+		else if (choice == 5) {
+			printf("Inorder traversal of tree is : ");
+			inorder(t, t->root);
+			printf("\n");
+		}
+		else if (choice == 6) {
+			printf("Postorder traversal of tree is : ");
+			postorder(t, t->root);
+			printf("\n");
+		}
+		else if (choice == 7) {
+			printf("Preorder traversal of tree is : ");
+			preorder(t, t->root);
+			printf("\n");
+		}
+		else if (choice == 8)break;
 	}
-	printf("%d\n",blackHieght(t));
-	preorder(t,t->root);
-	printf("\n");
-	postorder(t,t->root);
-	printf("\n");
-	inorder(t,t->root);
-	printf("\n");
-	printf("max = %d \n",maximum(t));
-	printf("min = %d \n",minimum(t));
-	search(t,5)?printf("found\n"):printf("Not found\n");
-	printPath(t,6);
-	delete(t,5);
-	inorder(t,t->root);
 	return 0;
 }
